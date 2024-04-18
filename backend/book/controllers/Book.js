d7c86bdaa5dd68b7b95a293fba42352c000e9536
MliliGenes <saad.elm.77@gmail.com>
2024-04-18 12:14:08 +0000

    book finished

--- backend/book/controllers/Book.js
+++ backend/book/controllers/Book.js
@@ -44,8 +44,50 @@ export const deleteBook = async (req, res) => {
   try {
     let jsonRes = { message: "success" };
     const { code } = req.params;
-    let isDeleted = await Book.findOneAndDelete({ code });
+    let deletedBook = await Book.findOneAndDelete({ code });
 
-    res.status(200).json(jsonRes);
+    if (deletedBook) {
+      jsonRes.message = "Book deleted successfully";
+      res.status(200).json(jsonRes);
+    } else {
+      jsonRes.message = "Book not found";
+      res.status(404).json(jsonRes);
+    }
+  } catch (err) {}
+};
+
+export const updateBook = async (req, res) => {
+  try {
+    let jsonRes = { message: "success", data: null };
+    const { code } = req.params;
+    const newBookData = req.body;
+
+    const oldBook = await Book.findOne({ code });
+
+    if (!oldBook) {
+      jsonRes.message = "Book not found";
+      return res.status(404).json(jsonRes);
+    }
+
+    const isSameBook =
+      JSON.stringify(oldBook.toObject()) === JSON.stringify(newBookData);
+
+    if (isSameBook) {
+      jsonRes.message = "No changes detected";
+      return res.status(200).json(jsonRes);
+    }
+
+    const updatedBook = await Book.findOneAndUpdate({ code }, newBookData, {
+      new: true,
+    });
+
+    if (updatedBook) {
+      jsonRes.message = "Book updated successfully";
+      jsonRes.data = updatedBook;
+      res.status(200).json(jsonRes);
+    } else {
+      jsonRes.message = "Failed to update book";
+      res.status(500).json(jsonRes);
+    }
   } catch (err) {}
 };
--- backend/book/routes/Book.js
+++ backend/book/routes/Book.js
@@ -11,6 +11,7 @@ const route = express.Router();
 route.get("/", getBooks);
 route.get("/:code", getBookById);
 route.post("/add-book", addBook);
+route.put("/add-book", updateBook);
 route.delete("/delete-book/:code", deleteBook);
 
 export default route;
