05d3723139900a2a4af3a8c40f8a3347f7a4f79d
Med-blbb <medblbbstudies@gmail.com>
2024-04-23 13:32:24 +0100

    fixed mail notification loan

--- backend/notification/app.js
+++ backend/notification/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { connect } from "amqplib";
 import nodemailer from "nodemailer"
+import axios from 'axios'
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(cors());
 dotenv.config();
 
 const port = process.env.PORT || 3003;
-const connectionString = process.env.MONGODB || "mongodb://localhost:27017/";
+// const connectionString = process.env.MONGODB || "mongodb://localhost:27017/";
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 var transporter = nodemailer.createTransport({
@@ -25,9 +26,9 @@ var transporter = nodemailer.createTransport({
 
 
 app.listen(port, () => console.log("server connected"));
-mongoose
-  .connect(connectionString + "Notifications")
-  .then(() => console.log("database connected"));
+// mongoose
+//   .connect(connectionString + "Notifications")
+//   .then(() => console.log("database connected"));
 
 const connection = await connect("amqp://localhost:5672");
 
@@ -80,41 +81,44 @@ channel.consume(queueLoans, async (message) => {
   let loanData = JSON.parse(message.content.toString());
   let client = loanData.client;
   let book = loanData.book;
-  let dateRetour = loanData.dateRetour ? loanData.dateRetour : "Not returned yet";
+  let dateRetour = loanData.dateRetour ||  "Not returned yet";
   let dateEmprunt = loanData.dateEmprunt;
   console.log("new loan taken:", client, book, dateRetour, dateEmprunt);
-  // let clientRecord = await axios.get("http://127.0.0.1:3001/api/" + client);
-  //   let dataClient = clientRecord.data;
+  let clientRecord = await axios.get("http://127.0.0.1:3001/api/" + client);
+    let dataClient = clientRecord.data;
 
-  //   let bookRecord = await axios.get("http://127.0.0.1:3000/api/" + book);
-  //   let dataBook = bookRecord.data;
-
-  //   if (!dataClient.data || !dataBook.data) {
-  //     jsonRes.message = "Client or book not found";
-  //     res.status(404).json(jsonRes);
-  //   }
-
-  // var mailOptions = {
-  //   from: 'medblbbstudies@gmail.com',
-  //   to: 'medblbbstudies@gmail.com',
-  //   subject: 'loan taken: ' + bookTitle,
-  //   html: `
-  //     <html>
-  //       <body>
-  //         <h2 style="color: #007bff;">A new loan has been taken:</h2>
-  //         <p><strong>Title:</strong> ${dataBook.tite}</p>
-  //         <p><strong>Description:</strong> ${dataBook.description}</p>
-  //         <p><strong>Author:</strong> ${dataBook.auteur}</p>
-  //       </body>
-  //     </html>`
-  // };
+    let bookRecord = await axios.get("http://127.0.0.1:3000/api/" + book);
+    let dataBook = bookRecord.data;
+    let   {data : clientInfo} = dataClient
+    let   {data : bookInfo} = dataBook
+    if (!dataClient.data || !dataBook.data) {
+      
+      console.log("Client or book not found");
+    }
+    console.log(dataClient , dataBook);
+  var mailOptions = {
+    from: 'medblbbstudies@gmail.com',
+    to: 'medblbbstudies@gmail.com',
+    subject: 'loan taken: ' + bookInfo?.titre,
+    html: `
+      <html>
+        <body>
+          <h2 style="color: #007bff;">A new loan has been taken:</h2>
+          <p><strong>Title:</strong> ${bookInfo?.titre}</p>
+          <p><strong>Description:</strong> ${bookInfo?.description}</p>
+          <p><strong>Author:</strong> ${bookInfo?.auteur}</p>
+          <p><strong>Client:</strong> ${clientInfo?.nom}</p>
+          <p><strong>Date emprunt:</strong> ${dateEmprunt}</p>
+        </body>
+      </html>`
+  };
   
-  // transporter.sendMail(mailOptions, function(error, info){
-  //   if (error) {
-  //     console.log('Erreur : ' + error);
-  //   } else {
-  //     console.log('Email sent: ' + info.response);
-  //   }
-  // });
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log('Erreur : ' + error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   channel.ack(message);
 });
